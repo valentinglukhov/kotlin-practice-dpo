c8e976be30131cd651cc552625adf1c59af84949
Валентин Глухов <glukhov_vy@vk.com>
2023-03-09 13:57:31 +0400

    fix(main): fix syntax remarks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 fun main() {
-    val phoneNumbers = enterNumbers(7)
+    var numbers = 0
+    loop@ do {
+        println("Введите количество номеров:")
+        numbers = readln().toIntOrNull() ?: continue@loop
+    } while (numbers <= 0)
+    val phoneNumbers = enterNumbers(numbers)
     println("Полный список номеров:")
     println(phoneNumbers.joinToString())
-    val filteredNumbers = phoneNumbers.filter { it.startsWith("+7")}
+    val filteredNumbers = phoneNumbers.filter { it.startsWith("+7") }
     println("Список номеров начинающиеся с +7")
     println(filteredNumbers.joinToString())
-    val setOfNumbers = filteredNumbers.toHashSet()
+    val setOfNumbers = phoneNumbers.toHashSet()
     println("Количество уникальных номеров: ${setOfNumbers.size}")
-    val sumNumbers = setOfNumbers.sumBy { it.length }
-    println("Длина всех номеров включая символы: ${sumNumbers}")
+    val sumNumbers = setOfNumbers.sumOf { it.length }
+    println("Длина всех номеров включая символы: $sumNumbers")
     val contacts = enterNames(setOfNumbers)
     println("Итоговый список контактов:")
     for (contact in contacts) {
@@ -16,19 +21,19 @@ fun main() {
     }
 }
 
-fun enterNames (phoneSet: HashSet<String>) : MutableMap<String, String> {
-    val phoneСontacts = mutableMapOf<String, String>()
+fun enterNames(phoneSet: HashSet<String>): MutableMap<String, String> {
+    val phoneContacts = mutableMapOf<String, String>()
     for (phone in phoneSet) {
         println("Введите имя для телефона: $phone")
         val name = readln()
-        phoneСontacts.put(phone, name)
+        phoneContacts.put(phone, name)
     }
-    return phoneСontacts
+    return phoneContacts
 }
 
-fun enterNumbers (n: Int) : MutableList<String> {
+fun enterNumbers(n: Int): MutableList<String> {
     val phoneNumbers = mutableListOf<String>()
-    if (n <= 0 || n.toInt() == null) return phoneNumbers
+    if (n <= 0) return phoneNumbers
     for (n in 1..n) {
         println("Введите номер телефона:")
         val number = readln()
